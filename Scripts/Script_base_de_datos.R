af9499cf412708a4c5d1978c978450a309f7ee7d
MiguetsuMARF <mrodriguez243@alumnos.uaq.mx>
2025-05-18 19:54:16 -0600

    Base de datos general

---
+++ Scripts/Script_base_de_datos.R
@@ -0,0 +1,11 @@
+
+### Script para metadatos de todas las bases de datos ###
+
+Datos_parasitos <- data.frame(
+  Parasito = c(), ## Aqui se agregan los parasitos que vamos a utilizar
+  Mecanismo_patogenicidad = c(),
+  Tipo_de_parasito = c(),
+  Intra_o_extra_cel = c(),
+  Localizacion_anatomica = c(),
+  Directo_Indirecto = c()
+)
