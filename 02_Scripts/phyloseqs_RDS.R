b14ef5958a6220ca470ca0da55f721f30baf7438
MiguetsuMARF <mrodriguez243@alumnos.uaq.mx>
2025-05-24 18:18:01 -0600

    scripts

--- 02_Scripts/phyloseqs_RDS.R
+++ 02_Scripts/phyloseqs_RDS.R
@@ -1,1 +1,18 @@
 library(phyloseq)
+
+ps_haplorchis <- readRDS("01_RowData/RDS_ps/ps_Haplorchis") ## Columna Parasito en sample_data
+
+ps_ascaris <- readRDS("01_RowData/RDS_ps/ps_ASCARIS") ## Columna Parasito en sample_data
+
+ps_opisthorchis <- readRDS("01_RowData/RDS_ps/ps_OPISTHORCHIS") ## Columna Parasito en sample_data
+
+ps_trichuris <- readRDS("01_RowData/RDS_ps/ps_TRICHURIS") ## Columna Parasito en sample_data
+
+ps_ancilostoma <- readRDS("01_RowData/RDS_ps/ps_ANCILOSTOMA") ## Columna Parasito en sample_data
+
+ps_necator <- readRDS("01_RowData/RDS_ps/ps_NECATOR") ## Columna Parasito en sample_data
+
+ps_taenia <- readRDS("01_RowData/RDS_ps/ps_TAENIA") ## Columna Parasito en sample_data
+
+ps_plasmodium <- readRDS("01_RowData/RDS_ps/ps_plasmodium") ## Columna Parasito en sample_data
+
