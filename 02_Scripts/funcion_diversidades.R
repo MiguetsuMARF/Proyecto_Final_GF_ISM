d2a9785246310255d4473f56c0d4c2e3728f2adf
Lysteria <sfigueroa13@alumnos.uaq.mx>
2025-05-24 18:45:52 -0600

    Hice muchas cosas, pero creo que ya jala

--- 02_Scripts/funcion_diversidades.R
+++ 02_Scripts/funcion_diversidades.R
@@ -1,28 +1,50 @@
-diversidades_parasitos <- function(vector_phyloseqs_varios) {
+library(vegan)
+library(phyloseq)
+
+diversidades_parasito <- function(lista_phyloseq) {
   resultados <- data.frame(
     ID = character(),
     Shannon = numeric(),
     Simpson = numeric(),
-    Chao = numeric()
+    Chao1 = numeric(),
+    stringsAsFactors = FALSE
   )
-  for (i in 1:length(vector_phyloseqs_varios)) {
-    ps <- vector_phyloseqs_varios[[i]]
+  
+  for (i in 1:length(lista_phyloseq)) {
+    ps <- lista_phyloseq[[i]]
     otu <- as.matrix(otu_table(ps))
+    
     if (taxa_are_rows(ps)) {
       otu <- t(otu)
     }
     
     shannon <- mean(diversity(otu, index = "shannon"))
     simpson <- mean(diversity(otu, index = "simpson"))
-    chao <- mean(estimateR(otu)["Chao", ])
+    
+    estimadores <- estimateR(otu)
+    if ("Chao1" %in% rownames(estimadores)) {
+      chao1 <- mean(estimadores["Chao1", ])
+    } else {
+      chao1 <- NA
+    }
     
     resultados <- rbind(resultados, data.frame(
       ID = names(lista_phyloseq)[i],
       Shannon = shannon,
       Simpson = simpson,
-      Chao = chao
+      Chao1 = chao1
     ))
   }
   
   return(resultados)
 }
+
+
+prueba1<- list(phylo_ancylostoma,phylo_ascaris)
+
+names(prueba1) <- paste0("phylo", seq_along(prueba1))
+
+names(prueba1)
+diversidades_parasito(prueba1)
+
+
