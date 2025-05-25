c58802d752c2959b22f9e3003b42cb441fb4850e
MiguetsuMARF <mrodriguez243@alumnos.uaq.mx>
2025-05-24 22:17:40 -0600

    uniendoooooo

--- 02_Scripts/uniendo_phyloseq.R
+++ 02_Scripts/uniendo_phyloseq.R
@@ -30,3 +30,47 @@ A7 <- as.data.frame(otu_table(ps_taenia))
 A8 <- as.data.frame(otu_table(ps_trichuris))
 A9 <- as.data.frame(otu_table(ps_control))
 A10 <- as.data.frame(otu_table(ps_schistosoma))
+
+A1$Parasito <- sample_data(ps_haplorchis)$Parasito
+A2$Parasito <- sample_data(ps_ancilostoma)$Parasito
+A3$Parasito <- sample_data(ps_ascaris)$Parasito
+A4$Parasito <- sample_data(ps_necator)$Parasito
+A5$Parasito <- sample_data(ps_opisthorchis)$Parasito
+A6$Parasito <- sample_data(ps_plasmodium)$Parasito
+A7$Parasito <- sample_data(ps_taenia)$Parasito
+A8$Parasito <- sample_data(ps_trichuris)$Parasito
+A9$Parasito <- sample_data(ps_control)$Parasito
+A10$Parasito <- sample_data(ps_schistosoma)$Parasito
+
+library(tidyverse)
+
+A <- bind_rows(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10)
+
+A <- A[,c("Parasito", setdiff(names(A), "Parasito"))]
+A$Parasito
+
+Final <- data.frame()  
+
+a <- c("Haplorchis taichui", "Ancilostoma", "Ascaris", "Necator",
+       "Opisthorchis", "Plasmodium", "Taenia Saginata", "Trichuris", "Control", "Schistosoma")
+
+for (i in 1:10) {
+  b <- a[i]
+  z <- A[A$Parasito == b, ]  
+  f <- c() 
+  for (u in 2:dim(A)[2]) {
+    e <- mean(z[, u])  
+    f <- c(f, e)        
+  }
+  fd <- as.data.frame(t(f)) 
+  fd$Parasito <- b           
+  Final <- bind_rows(Final, fd)  
+}
+
+dim(Final)
+
+Final <- Final[, c(ncol(Final), 1:(ncol(Final)-1))]
+View(Final)
+a <- c(1:4)
+b <- c(5:8)
+
