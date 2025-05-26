0ac1c55508f2c55dac8d4bf65e4e0eec861418e6
MiguetsuMARF <mrodriguez243@alumnos.uaq.mx>
2025-05-26 12:29:56 -0600

    Comentando

--- 02_Scripts/Script_editando_metadatos.R
+++ 02_Scripts/Script_editando_metadatos.R
@@ -2,6 +2,10 @@
 
 library(phyloseq)
 
+## primero cargamos los phyloseqs que vamos a editar
+
+# debido a que no tienen los datos necesarios para hacer el analisis posterior vamos a agregarlo.
+
 psH1 <- readRDS("01_RowData/RDS_ps/ps_helmints")
 
 psH2 <- readRDS("01_RowData/RDS_ps/ps_helmints2")
@@ -14,12 +18,15 @@ View(sample_data(psH2))
 
 View(sample_data(psP))
 
-datos2 <- read.csv("../../journal.pntd.0010491.s002.xlsx - Supplmentary Table 1.csv")
+# para el phyloseq de helmintos 1 vamos a utilizar otros metadatos, pero como estos no tienen los accesos para alinear en el phyloseq debemos hacerlo a mano
+
+datos2 <- read.csv("01_RowData//journal.pntd.0010491.s002.xlsx - Supplmentary Table 1.csv")
 View(datos2)
 datos2[,11]
 
 rownames(datos2) <- datos2$Sample.id
 
+# ahora si vamos a crear un nuevo onjeto que tenga los metadatos originales y otro que tenga los nuevos meta datos, mientras que en la segunda se van a asignar nuevas variables vacias que se van a rellenar con el orden correcto.
 psH1_meta <- sample_data(psH1)
 psH1_meta1 <- list()
 psH1_meta1$"SampleName" <- psH1_meta[,"Sample.Name"]
@@ -35,11 +42,13 @@ v <- c(psH1_meta1$SampleName2)
 
 library(stringr)
 
+# Aqui se va a realizar un ciclo for para buscar coincidencias entre el sample name original y el de los nuevos metadatos, esto con la funcion str?detect que busca coincidencia entre patrones.
 for (i in 1:93) {
   a <- as.character(psH1_meta1$SampleName[i]) 
   psH1_meta1$"Ordenado"[which(str_detect(psH1_meta1$SampleName2, a))] <- row.names(psH1_meta1$SampleName[i])
 }
 
+# y posteriormente vamos a crear la nueva columna con los sample names y los parasitos re ordenados con el orden original
 x <- 0
 for (i in 1:93) {
   a <- as.character(psH1_meta1$SampleName[i]) 
@@ -82,22 +91,4 @@ View(sample_data(psH1f))
 psH2f <- subset_samples(psH2, sample_data(psH2)$disease != "negative")
 View(sample_data(psH2f))
 
-A1 <- as.data.frame(otu_table(psH1f))
-A2 <- as.data.frame(otu_table(psH2f))
-A3 <- as.data.frame(otu_table(psP))
 
-A1$Parasito <- sample_data(psH1f)$Parasito
-A2$Parasito <- sample_data(psH2f)$disease
-A3$Parasito <- rep("Plasmodium", dim(A3)[1])
-
-View(A3)
-
-install.packages("tidyverse")
-library(tidyverse)
-
-A <- bind_rows(A1,A2,A3)
-
-dim(A)
-
-A <- A[,c("Parasito", setdiff(names(A), "Parasito"))]
-View(A)
